1e8fa5d3c534c6863126e1dcbb263b58576b055c
Faroeq <alves_1044@hotmail.com>
2022-03-05 20:42:55 +0100

    Squashed commit of the following:
        *Display total amount after userinput
        *Rename hook handleInputChange to handleReceiptChange
        *Remove comment
        *Add useInputChangeHook
        *add comments, add plan.txt
        *Replace hardcoded values with vars

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import PercentInput from "./components/PercentInput";
 import OutputField from "./components/OutputField";
 import personIcon from "./images/icon-person.svg";
 
+import { useInputChange } from "./components/useInputChange";
+
 function App() {
+  const [receipt, handleReceiptChange] = useInputChange();
+
   return (
     <div className="text-gray-600 App bg-lightergrayishcyan">
+      {JSON.stringify(receipt)}
       <div className="container mx-auto max-w-[375px] px-4">
-        <ul className="py-8 text-2xl font-extrabold uppercase opacity-75 text-verydarkcyan">
+        <ul className="py-10 text-2xl font-extrabold uppercase opacity-75 text-verydarkcyan">
           <li>s p l i</li>
           <li>t t e r</li>
         </ul>
         <div className="flex p-4 bg-white rounded-2xl">
-          <div className="grid w-full grid-cols">
+          <div className="grid w-full gap-3 grid-cols">
             <InputLabel>Bill</InputLabel>
             <OutputField>
-              <span className="text-left text-gray-400">$ </span>
-              <span className="text-right">142.55</span>
+              <input
+                type="number"
+                name="bill"
+                className="inline text-right bg-inherit"
+                onChange={handleReceiptChange}
+                placeholder="whats the bill?"
+              />
             </OutputField>
 
             <InputLabel>Select Tip % </InputLabel>
@@ -29,29 +39,45 @@ function App() {
               <PercentInput>15%</PercentInput>
               <PercentInput>25% </PercentInput>
               <PercentInput>50%</PercentInput>
-              <div className="p-4 text-2xl font-bold bg-slate-100">Custom </div>
+              <div className="p-2 text-2xl font-bold text-gray-500 rounded-md bg-slate-100">
+                Custom
+              </div>
             </div>
 
             <InputLabel>Number of People </InputLabel>
             <OutputField cName="grid grid-cols-2">
-              <div className="">
+              <div className="hidden p-2">
                 <img className="" src={personIcon} alt="" />
               </div>
               <div className="">
-                <span className="text-right">5</span>
+                <input
+                  type="number"
+                  name="numberOfPeople"
+                  className="text-right bg-inherit"
+                  onChange={handleReceiptChange}
+                  placeholder="2.."
+                />
               </div>
             </OutputField>
-            <div className="grid grid-cols-2 gap-2 p-6 rounded-lg bg-verydarkcyan">
+            <div className="grid grid-cols-2 gap-2 p-6 pt-10 rounded-lg bg-verydarkcyan">
               <ReceiptRow>
                 Tip Amount <br></br>
-                <span className="text-gray-400">/ person</span>
+                <span className="text-sm text-gray-400">/ person</span>
               </ReceiptRow>
-              <ReceiptNum>$4.27</ReceiptNum>
+              <ReceiptNum>${receipt.tipPerPerson | 0}</ReceiptNum>
               <ReceiptRow>
                 <div>Total </div>
                 <span className="text-gray-400">/ person</span>
               </ReceiptRow>
-              <ReceiptNum>32.79</ReceiptNum>
+
+              <ReceiptNum>
+                {/* receipt divided by numberOfPeople otherwhise calculate nothing  */}
+                ${(receipt.bill / receipt.numberOfPeople) | ""}
+              </ReceiptNum>
+
+              <button className="w-full col-span-2 p-2 pt-4 text-xl font-bold uppercase rounded-md text-verydarkcyan bg-darkgrayishcyan">
+                reset
+              </button>
             </div>
           </div>
         </div>
