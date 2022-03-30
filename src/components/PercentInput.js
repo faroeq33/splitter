a9c0a23786208eb2782d0e96f637621ca4b9586a
Faroeq <alves_1044@hotmail.com>
2022-03-30 20:01:39 +0200

    Remove consolelog

--- src/components/PercentInput.js
+++ src/components/PercentInput.js
@@ -1,15 +1,18 @@
 export default function PercentInput(props) {
   // if extendedClassName prop is available use it, if not use empty string
-  console.log(props.extendClassname);
   const extendedClassnames = props.extendClassname
     ? props.extendedClassnames
     : "failed exclassname ";
 
   return (
+    <>
     <button
       className={`bg-verydarkcyan rounded-md p-2 text-3xl font-bold text-white focus:bg-darkgrayishcyan focus:text-verydarkcyan ${props.extendClassname}`}
+    onClick={props.onClick}
     >
       {props.children}
     </button>
+    </>
+
   );
 }
